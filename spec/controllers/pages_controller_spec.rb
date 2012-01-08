120b00a24c1d18e6afd274bbf45d178e4db43d89
Warren Held <warrenheld@gmail.com>
2012-01-08 15:18:38 -0600

    Dynamic html titles

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -22,7 +22,7 @@ describe PagesController do
       end
 
       it "should have the right title" do
-        get 'home'
+        get 'contact'
         response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
       end
     end
@@ -34,8 +34,8 @@ describe PagesController do
       end
 
     it "should have the right title" do
-        get 'home'
-        response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
+        get 'about'
+        response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | About")
     end
   end
 end
