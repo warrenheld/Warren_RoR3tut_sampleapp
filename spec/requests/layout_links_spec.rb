b91f0328c4bc71e722345cce4a2e7b5de70da30d
Warren Held <warrenheld@gmail.com>
2012-01-11 13:40:21 -0600

    Finished layout and routes

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -17,9 +17,14 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "About")
   end
 
-    it "should have a Help page at '/help'" do
+  it "should have a Help page at '/help'" do
     get '/help'
     response.should have_selector('title', :content => "Help")
+  end
+
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
   end
 
 end
